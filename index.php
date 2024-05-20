eaf6eb7b9ea070ea949b3a502a639fcd0fafaa5d
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-20 22:24:47 +0200

    feat: added style - response.php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+   <!-- Link Bootstrap -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <!-- /Link Bootstrap -->
 
    <title>PHP - Form Bad Words</title>
 
@@ -16,14 +18,14 @@
 
    <div class="container-sm">
 
-      <h1 class="my-3 text-center">Censor a text</h1>
+      <h1 class="my-3 text-center text-primary">Censor a text</h1>
 
       <div class="row justify-content-center">
 
-         <form class="col-4" action="response.php" method="GET">
+         <form class="col-8" action="response.php" method="GET">
 
             <!-- Paragraph Input -->
-            <div class="mb-3">
+            <div class="mb-3 border rounded-3 p-2">
 
                <label for="user-paragraph" class="form-label">
                   Enter here a paragraph.
@@ -35,7 +37,7 @@
             <!-- /Paragraph Input -->
 
             <!-- Word Input -->
-            <div class="mb-3">
+            <div class="mb-3 border rounded-3 p-2">
 
                <label for="user-word" class="form-label">
                   Insert here a word to censor.
--- response.php
+++ response.php
@@ -4,7 +4,6 @@ $user_paragraph = $_GET["paragraph"];
 
 $user_word = $_GET["bad-word"];
 
-
 ?>
 
 
@@ -16,37 +15,49 @@ $user_word = $_GET["bad-word"];
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+   <!-- Link Bootstrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <!-- /Link Bootstrap -->
+
    <title>PHP - Response Bad Words</title>
 
 </head>
 
 <body>
 
-   <h1>RESULTS:</h1>
+   <div class="container-sm">
 
-   <!-- Output Paragraph as written -->
-   <div>
+      <h1 class="my-3 text-center text-primary">Result of your censure:</h1>
 
-      <h2>Paragraph as written:</h2>
+      <div class="row g-2 justify-content-center">
 
-      <p> <?php echo $user_paragraph; ?> </p>
+         <!-- Output Paragraph as written -->
+         <div class="col-8 border rounded-4 p-3">
+   
+            <h2>Paragraph as written:</h2>
+   
+            <p> <?php echo $user_paragraph; ?> </p>
+   
+            <h5>Length: <em><?php echo strlen($user_paragraph); ?></em> characters</h5>
+   
+         </div>
+         <!-- /Output Paragraph as written -->
+   
+         <!-- Output censored Paragraph-->
+         <div class="col-8 border rounded-4 p-3">
+   
+            <h2>Paragraph censored:</h2>
+   
+            <p> <?php echo str_replace($user_word, "***", $user_paragraph); ?> </p>
+   
+            <h5>Length: <em><?php echo strlen(str_replace($user_word, "***", $user_paragraph)); ?></em> characters</h5>
+   
+         </div>
+         <!-- /Output censored Paragraph-->
 
-      <h5>Length:  <em><?php echo strlen($user_paragraph); ?></em> characters</h5>
+      </div>
 
    </div>
-   <!-- /Output Paragraph as written -->
-
-   <!-- Output censored Paragraph-->
-   <div>
-
-      <h2>Paragraph censored:</h2>
-
-      <p> <?php echo str_replace($user_word,"***", $user_paragraph); ?> </p>
-
-      <h5>Length:  <em><?php echo strlen(str_replace($user_word,"***", $user_paragraph)); ?></em> characters</h5>
-
-   </div>
-   <!-- /Output censored Paragraph-->
 
 </body>
 
